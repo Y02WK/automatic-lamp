cefc8f39a14e434b51e7c8ab78cf8f471dd52e98
Y02WK <59220086+Y02WK@users.noreply.github.com>
2022-03-08 14:04:30 +0800

    Recommitting release repo

--- protoType/src/app/directives/max-length.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { MaxLengthDirective } from './max-length.directive';
-
-describe('MaxLengthDirective', () => {
-  it('should create an instance', () => {
-    const directive = new MaxLengthDirective();
-    expect(directive).toBeTruthy();
-  });
-});
